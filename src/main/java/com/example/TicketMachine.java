344ee996654a13b96934c21c95852fe2ce884102
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-09 08:38:11 +0000

    simple factory pattern

--- src/main/java/com/example/TicketMachine.java
+++ src/main/java/com/example/TicketMachine.java
@@ -2,8 +2,11 @@ package com.example;
 
 public class TicketMachine {
 
-    Ticket createTicket() {
-        return new BusTicket();
+    Ticket createTicket(String ticketType) {
+        if(ticketType.equalsIgnoreCase("Bus")) {
+            return new BusTicket();
+        }
+        return new TrainTicket();
     }
 
 
