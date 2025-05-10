011bd365e2c859d3ba7b36f7945884d6ffe555cd
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-10 12:22:51 +0000

    Flyweight

---
+++ src/main/java/com/example/OrderFactory.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderFactory {
+
+  private static final Map<String, Order> orderCache = new HashMap<>();
+
+  public static Order createOrder(String name) {
+    // if(orderCache.containsKey(name)) {
+    //   return orderCache.get(name);
+    // }
+    // var order = new Order(name);
+    // orderCache.put(name, order);
+    // return order;
+    return orderCache.computeIfAbsent(name, Order::new);
+  }
+  
+}
