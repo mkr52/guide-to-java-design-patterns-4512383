f32e912b13207746d65c67cfa13e66a07f1d1a3a
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-10 07:33:05 +0000

    Bridge

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,11 +3,14 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        var hoodie = new Hoodie();
-        hoodie.getType();
 
-        var shirt = new Shirt();
+        var hoodie = new Hoodie(new ChildSize());
+        hoodie.getType();
+        hoodie.getSize().get();
+
+        var shirt = new Shirt(new AdultSize());
         shirt.getType();
+        shirt.getSize().get();
     }
 
 }
